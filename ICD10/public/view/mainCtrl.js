5a7fc57ba052639a01fc268094d2e98b2ce3ae31
Edgar Wright <MacPro@Edgars-MacBook-Pro.local>
2015-10-29 19:03:22 -0600

    test

--- ICD10/public/view/mainCtrl.js
+++ ICD10/public/view/mainCtrl.js
@@ -9,8 +9,7 @@
         face : imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
-        when: '3:08PM',
-        notes: " I'll be in your neighborhood doing errands"
+        when: '3:08PM'
       },
       {
         face : imagePath,
