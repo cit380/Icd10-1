334f823c606da9bb5aac301ebba66df6a43ededb
Edgar Wright <MacPro@Edgars-MacBook-Pro.local>
2015-11-06 22:11:52 -0700

    Db Changes

--- ICD10/public/view/mainCtrl.js
+++ ICD10/public/view/mainCtrl.js
@@ -1,15 +1,25 @@
 (function(){
 	angular.module('ICD10').controller('mainCtrl', function($scope, mainService){
 
-      function getCodes(data){
-        mainService.getQuery(data)          
-      }
+        function query(){
+			mainService.getQuery().then(function(res){
+				console.log('inside', res)
+				// return res[0].data;
+				$scope.codes = res
+			}, function(err){
+				console.log('error with get', err)
+			});
+		};
+		$scope.runRequest = query();
+        
+    var imagePath = 'img/list/60.jpeg';
     $scope.messages = [
       {
         face : imagePath,
         what: 'Brunch this weekend?',
         who: 'Min Li Chan',
-        when: '3:08PM'
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
       },
       {
         face : imagePath,
--- ICD10/public/view/mainService.js
+++ ICD10/public/view/mainService.js
@@ -1,10 +1,11 @@
 angular.module('ICD10').service('mainService', function($q, $http){
 	
 	this.getQuery = function(data){
+		console.log("running")
 		var dfd = $q.defer()
 		$http({
 			method: 'GET',
-			url: '',
+			url: 'https://api.aquahost.com/codes/icd10/2013/a92-30.json',
 			data: data,
 		}).then(function(res){
 			console.log('Get Res:', res)
